d1c07ce0e3b6e5190b4a874bfb8854d95dfc9ff9
maarcf <marr.cairo@gmail.com>
2022-11-02 00:25:59 -0300

    add sass: mixins, variables; add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container'>
+        <h1 className='header__title'>Weather App</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
